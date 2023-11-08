1e41d4b8f5ded4a093aedc3899216dfddbbd2f5d
AyushiChopra02 <139689074+AyushiChopra02@users.noreply.github.com>
2023-11-08 22:54:21 +0530

    add

--- Bubbling.js
+++ Bubbling.js
@@ -12,5 +12,5 @@ const makeRandColor = () => {
     const r = Math.floor(Math.random() * 255);
     const g = Math.floor(Math.random() * 255);
     const b = Math.floor(Math.random() * 255);
-    return `rgb(${r} , ${g} , ${b})`;
+    return `rgb(${r} , ${g} ,${b})`;
 }
